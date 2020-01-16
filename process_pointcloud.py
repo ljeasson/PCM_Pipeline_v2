835b2cc1b1c0dc1f73ac5d5e5fd09246bce05acf
Lee Easson <ljeasson@gmail.com>
2020-01-16 15:38:59 -0800

    Add point cloud processing scripts

---
+++ process_pointcloud.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+
+import os
+import glob
+import sys, getopt
+
+from segment_pointcloud import segment_point_cloud
+from create_heightmap import create_heightmap
+from merge_tiles import merge_tiles
+
+def main(argv):
+    #print('Number of arguments:', len(sys.argv), 'arguments.')
+    #print('Argument List:', str(sys.argv))
+    
+    directory = ''
+
+    try:
+        opts, args = getopt.getopt(argv,"hi:",["ifile="])
+    except getopt.GetoptError:
+        print('USAGE: process_pointcloud.py -i <input file or directory>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h' or opt == '--help':
+            print('USAGE: process_pointcloud.py -i <input file or directory>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            directory = arg
+
+    # If input ends with .laz
+    if directory.endswith(".laz"):
+        print('Incorrect file format\nUse .las or .laz point clouds')
+        sys.exit()
+    
+    print('Input: ', directory, '\n')
+
+    # IF INPUT IS A POINT CLOUD
+    if directory.endswith(".las"):
+        # Get point cloud file name without ".las"
+        directory_no_ext = directory[ : directory.index(".")]
+
+        # Segment Point Cloud into vegetation and ground
+        segment_point_cloud(directory, directory_no_ext)
+    
+    # IF INPUT IS A DIRECTORY
+    else:
+        # Merge directory of tiles
+        merge_tiles(directory, 0.5)
+
+        merged_directory = ""
+
+        for dirpath,_,filenames in os.walk(directory+"\merged"):
+            for f in filenames:
+                merged_directory = os.path.abspath(os.path.join(dirpath, f))
+        
+        merged_directory_no_ext = merged_directory[: merged_directory.index(".")]
+
+        # Segment Point Clouds into vegetation and ground
+        segment_point_cloud(merged_directory, merged_directory_no_ext)
+    
+    # Get point clouds
+    base_point_cloud, ground_point_cloud, veg_point_cloud, veg_point_cloud_subsampled = "",[],[],[]
+    base_point_cloud_txt, ground_point_cloud_txt, veg_point_cloud_txt, building_point_cloud_txt = "","","",""
+    for file in os.listdir(os.listdir(directory+"\merged")):
+        if file.endswith(".las"):
+            if "_ground.las" in file: ground_point_cloud.append(file)
+            elif "_vegetation.las" in file: veg_point_cloud.append(file)
+            elif "_vegetation_subsampled" in file: veg_point_cloud_subsampled.append(file)
+            else: base_point_cloud = file
+        if file.endswith(".txt"):
+            if "_ground.txt" in file: ground_point_cloud_txt = file
+            elif "_vegetation.txt" in file: veg_point_cloud_txt = file
+            elif "_building.txt" in file: building_point_cloud_txt = file
+            else: base_point_cloud_txt = file
+
+    print(base_point_cloud, base_point_cloud_txt)
+    print(ground_point_cloud, ground_point_cloud_txt)
+    print(veg_point_cloud, veg_point_cloud_txt, veg_point_cloud_subsampled)
+    print(building_point_cloud_txt)
+
+    # Create heightmap from Ground point cloud
+    #fileName_ground = input_point_cloud[:input_point_cloud.index(".")]
+    #create_heightmap(ground_point_cloud, fileName_ground)
+    
+    # Open UE4 Editor with PCM Pipeline
+    print("OPENING UE4 EDITOR WITH PCM PIPELINE")
+    #os.system("UE4Editor 'D:\Users\Lee\Unreal Projects\PCM_PIpeline_v2\PCM_PIpeline_v2.uproject'")
+    print("DONE\n")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ segment_pointcloud.py
@@ -0,0 +1,49 @@
+import os
+import glob
+
+def segment_point_cloud(directory, directoryNoExt, denoise=False, interpolation=False, interpolation_factor=0.0):
+    print("RUNNING LAS2TXT")
+    las2txt_command = "las2txt -i " + str(directory) + " -o " + str(directoryNoExt) + ".txt -parse xyzic -sep comma"
+    print(las2txt_command)
+    os.system(las2txt_command)
+    print("DONE\n")
+
+    print("POPULATE GROUND, VEG, ETC TXT FILES")
+    with open(str(directoryNoExt) + ".txt", "rt") as fin:
+        with open(str(directoryNoExt) + "_ground.txt", "wt") as fout_G: #Ground
+            with open(str(directoryNoExt) + "_vegetation.txt", "wt") as fout_V: #Vegetation
+                with open(str(directoryNoExt) + "_building.txt", "wt") as fout_B: #Building
+                    with open(str(directoryNoExt) + "_building.txt", "wt") as fout_U: #Unclassified
+                        for line in fin:
+                            line_split = line.split(',')
+                            if line_split[4] == '1\n':
+                                fout_U.write(line)
+                            if line_split[4] == '2\n':
+                                fout_G.write(line)
+                            if line_split[4] == '3\n' or line_split[4] == '4\n' or line_split[4] == '5\n':
+                                fout_V.write(line)
+                            if line_split[4] == '6\n':
+                                fout_B.write(line)
+                        
+    print("DONE\n")
+
+    print("RUNNING TXT2LAS FOR VEGETATION")
+    os.system("txt2las -i " + str(directoryNoExt) + "_vegetation.txt -o " + str(directoryNoExt) + "_vegetation.las -parse xyzic")
+    if denoise: os.system("lasnoise -i " + str(directoryNoExt) + "_vegetation.las -o "+ str(directoryNoExt) + "_vegetation_no_noise.las -remove_noise -ignore_class 2")
+    print("DONE\n")
+
+    print("RUNNING TXT2LAS FOR GROUND")
+    os.system("txt2las -i " + str(directoryNoExt) + "_ground.txt -o " + str(directoryNoExt) + "_ground.las -parse xyzic")
+    if denoise: os.system("lasnoise -i " + str(directoryNoExt) + "_ground.las -o "+ str(directoryNoExt) + "_ground_no_noise.las -remove_noise -ignore_class 2")
+    print("DONE\n")
+
+    if interpolation:
+        print("RUNNING LAS2DEM FOR GROUND")
+        os.system("las2dem -i " + str(directoryNoExt) + "_ground.las -o "+ str(directoryNoExt) + "_ground.laz -kill " + str(interpolation_factor))
+        print("RUNNING LASZIP FOR GROUND")
+        os.system("laszip -i " + str(directoryNoExt) + "_ground.laz")
+        print("RUNNING LASTHIN FOR GROUND")
+        os.system("lasthin -i " + str(directoryNoExt) + "_ground.las -o " + str(directoryNoExt) + "_ground_thinned.las -highest -step 2.0 -adaptive 0.2 5.0")
+        print("RUNNING LAS2TXT FOR GROUND")
+        os.system("las2txt -i " + str(directoryNoExt) + "_ground_thinned.las -o " + str(directoryNoExt) + "_ground_thinned.txt -parse xyzic -sep comma")
+        print("DONE\n")
